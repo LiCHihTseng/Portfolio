7935b1c6bc338e05de25a62ce6a2a9843b97dd53
LiChihTseng <lichih.tseng@uqconnect.edu.au>
2024-09-30 22:09:28 +1000

    try to change to scss

--- my-first-portfolio/src/App.js
+++ my-first-portfolio/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './app.scss';
 import {NavBar} from './components/NavBar';
 import {Banner} from './components/Banner';
 import {CardView} from './components/CardView';
